480b5a2186f0f797a0fa5458cd3238149c4e3e8f
Marcuss17 <eduardo.medina@utec.edu.pe>
2019-10-10 17:31:45 -0500

    Done

---
+++ LinkedList.h
@@ -0,0 +1,101 @@
+//
+// Created by eduardo on 10/10/19.
+//
+
+#ifndef CS1103_PC2_201902_LINKEDLIST_H
+#define CS1103_PC2_201902_LINKEDLIST_H
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using std::vector; using std::string; using std::ifstream;
+
+template<class T>
+class LinkedList{
+private:
+    struct node{
+        T val;
+        node* next;
+    };
+public:
+    node* tail = nullptr;
+    node* head = nullptr;
+    T get_max() {
+        T mayor = head->val;
+        auto it = head;
+        while(it != nullptr){
+            if(it->val > mayor){
+                mayor = it->val;
+            }
+            it = it->next;
+        }
+        return mayor;
+    }
+
+    void insert (T item){
+        if(head == nullptr){
+            node* temp = new node();
+            temp->val = item;
+            temp->next = head;
+            head = temp;
+            tail = temp;
+        }
+        else{
+            node* temp = new node();
+            temp->val = item;
+            temp->next = head;
+            head = temp;
+        }
+    }
+
+    template<class M>
+    friend::std::ostream & operator << (std::ostream & out, const LinkedList<M> &);
+
+    template<typename ... Args>
+    void insert (Args ... items) {
+        vector <T> temp = {items...};
+        auto it = temp.begin();
+        while(it != temp.end()){
+            node* aux = new node();
+            aux->val = *it;
+            aux->next = head;
+            head = aux;
+            it++;
+        }
+    }
+
+    void load_from (const string& file_name){
+        ifstream file(file_name);
+        if (!file.is_open()){
+            std::cout<< "File is not open!";
+        }
+        while(head != nullptr){
+            head = head->next;
+        }
+        string fila;
+        while(getline(file,fila)){
+            node* temp = new node();
+            std::stringstream ss (fila);
+            string str;
+            getline(ss,str);
+            int num = stoi(str);
+            temp->val = num;
+            temp->next = head;
+            head = temp;
+        }
+    }
+};
+
+template<typename M>
+std::ostream & operator << (std::ostream & out, const LinkedList<M>& L){
+    auto it = L.head;
+    while(it != nullptr){
+        out << it->val << " ";
+        it = it->next;
+    }
+    return out;
+}
+
+#endif //CS1103_PC2_201902_LINKEDLIST_H
--- main.cpp
+++ main.cpp
@@ -1,30 +1,27 @@
-#include <iostream>
+#include "LinkedList.h"
 
-#include "heap.h"
-using namespace std;
+using std::cout; using std::cin; using std::endl;
+int main(){
+    LinkedList<int>L1;
 
-int main()
-{
-    heap<int> h;
-    load_from("data.txt", h);
-    cout << "heap" << endl;
-    cout << h;
-    h.insert(10);
-    h.insert(20);
-    h.insert(40);
-    h.insert(50);
-    h.insert(10);
-    cout << "heap" << endl;
-    cout << h;
-    cout << "heap" << endl;
-    cout << h;
-    cin >> h;
-    cout << "heap" << endl;
-    cout << h;
-    insert(h, 100, 200);
-    cout << "heap" << endl;
-    cout << h;
-    load_from("data.txt", h);
-    cout << h;
+    //Ingresa multiples elementos a la lista
+    L1.insert(2,3,74,5);
+
+    //Ingresa elemento por elemento a la lista
+    L1.insert(15);
+    L1.insert(35);
+
+    //Retorna el mayor elemento de la lista
+    cout <<"El mayor elemento del la lista es: " << endl;
+    cout << L1.get_max();
+    cout << endl;
+
+    //Lee los elementos desde un archivo
+    L1.load_from("/home/eduardo/Desktop/Utec/Poo2/practica-calificada-2-Marcuss17/cmake-build-debug/data.txt");
+
+    //Imprimir los elementos de la lista, sobrecarga de  <<
+    cout << "Los elementos de la lista son: " << endl;
+    cout << L1;
+
     return 0;
 }
